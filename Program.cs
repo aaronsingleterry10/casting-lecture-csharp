35f69a4b8a57fc339e62adde2a829e21eadf3002
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-29 21:22:04 -0500

    closing project

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,10 @@ namespace Casting
     {
         static void Main(string[] args)
         {
+            Shape shape = new Text();
+            Text text = (Text)shape;
+            Text anotherText = new Text();
+            text.
         }
     }
 }
